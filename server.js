d6987aa7583ec04819a8992f7e952d6c4872dcc0
mikestegall <mike@dispo.fun>
2023-11-09 21:23:38 -0600

    added user and auth api calls

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 const express = require("express");
+const connectdb = require('./config/db');
 
 const app = express();
+connectdb();
 
 app.get("/", (req, res) => res.send("API running"));
+
+app.use(express.json({ extended: false }));
+
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/post', require('./routes/api/post'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
 
 const PORT = process.env.PORT || 6000;
 
